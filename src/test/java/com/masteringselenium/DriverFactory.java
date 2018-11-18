621b2671f86f20bed4872ed922b0b33951a1e2a1
JUVER MALPARTIDA <JUVERCASTRO@hotmail.com>
2018-11-18 11:12:04 -0500

    ju

--- src/test/java/com/masteringselenium/DriverFactory.java
+++ src/test/java/com/masteringselenium/DriverFactory.java
@@ -2,20 +2,31 @@ package com.masteringselenium;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class DriverFactory {
-    private  static ThreadLocal<WebDriverThread> driverThread;
+
+    private static List<WebDriverThread> webDriverThreadPool = Collections.synchronizedList(new ArrayList<WebDriverThread>());
+    private static ThreadLocal<WebDriverThread> driverThread;
+
     public static WebDriver getDriver() {
         return driverThread.get().getDriver();
     }
 
     @BeforeSuite
     public static void BeforeSuite(){
+        System.out.println("BEFORE SUITE!!!!");
         driverThread = new ThreadLocal<WebDriverThread>(){
             @Override
             protected WebDriverThread initialValue(){
+                System.out.println("CREATE DRIVER");
                 WebDriverThread webDriverThread = new WebDriverThread();
+                webDriverThreadPool.add(webDriverThread);
                 return  webDriverThread;
             }
         };
@@ -23,6 +34,13 @@ public class DriverFactory {
 
     @AfterMethod
     public static void quitDriver() throws Exception {
-        driverThread.get().quitDriver();
+        getDriver().manage().deleteAllCookies();
+    }
+
+    @AfterSuite
+    public static void AfterSuite(){
+        for (WebDriverThread webDriverThread : webDriverThreadPool) {
+            webDriverThread.quitDriver();
+        }
     }
 }
